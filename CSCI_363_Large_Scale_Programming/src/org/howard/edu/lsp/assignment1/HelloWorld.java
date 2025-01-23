124708ab042770627cb5f30c02acd2c1bbf36690
Vikash Rivers <vikashrivers18@gmail.com>
2025-01-23 02:43:10 -0500

    Adding Hello World

--- CSCI_363_Large_Scale_Programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ CSCI_363_Large_Scale_Programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,13 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-
+	
+	/**
+	 * Vikash Rivers
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
 
 	}
 
